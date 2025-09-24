ac5bfdecee36153399d48ead927b207bf1b79fb3
Abdulloh Umar <iniautok@gmail.com>
2025-09-24 08:56:46 +0700

    hero finish

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 const themeBtn = document.getElementById('theme-btn');
 const themeIcon = document.querySelector('.theme-icon');
+const typingText = document.getElementById('typing-text')
+
+
+// initialize the website 
+document.addEventListener('DOMContentLoaded', function() {
+    initializeTypingAnimation();
+    initializeParticles();
+});
 
 // Mobile nav toggle
 hamburger.addEventListener('click', function(){
@@ -29,3 +37,68 @@ themeBtn.addEventListener('click', function () {
     const icons = ['🌈', '💙', '💚'];
     themeIcon.textContent = icons[currentTheme];
 });
+
+// Typing Animation
+function initializeTypingAnimation() {
+    const texts = [
+        'Frontend Developer',
+        'Backend Developer',
+        'UI/UX Designer',
+        'Cyber Security Enthusiast'
+    ];
+
+    let textIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function typeText() {
+        const currentText = texts[textIndex];
+
+        if(isDeleting) {
+            typingText.textContent = currentText.substring(0, charIndex - 1);
+            charIndex--;
+        } else {
+            typingText.textContent = currentText.substring(0, charIndex + 1);
+            charIndex++;
+        }
+
+        let typeSpeed = isDeleting ? 50 : 100;
+
+        if(!isDeleting && charIndex === currentText.length){
+            isDeleting = true;
+            typeSpeed = 2000;
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            typeSpeed = 500;
+            textIndex = (textIndex + 1) % texts.length;
+        }
+
+        setTimeout(typeText, typeSpeed);
+    }
+    typeText();
+}
+
+// Particles Animation
+function initializeParticles() {
+    const particlesContainer = document.getElementById('particles');
+    const particleCount = 70;
+
+    for (let i = 0; i < particleCount; i++){
+        createParticles();
+    }
+
+    function createParticles() {
+        const particle = document.createElement('div');
+        particle.className = 'particle';
+        particle.style.left = Math.random() * 100 + '%';
+        particle.style.animationDelay = Math.random() * 6 + 's';
+        particle.style.animationDuration = (Math.random() * 3 + 3) + 's';
+        particlesContainer.appendChild(particle);
+
+        // Remove and Recreate particle
+        setTimeout(()=> {
+            particle.remove();
+            createParticles();
+        }, 6000)
+    }
+}
